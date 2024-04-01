3fbe9379adb9f4765bd322087a1888bb6f02a161
qmlx <2300604302@qq.com>
2024-04-01 10:31:27 +0800

    初始化

---
+++ src/main/java/com/qmlx/springbootinit/exception/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.qmlx.springbootinit.exception;
+
+import com.qmlx.springbootinit.common.BaseResponse;
+import com.qmlx.springbootinit.common.ErrorCode;
+import com.qmlx.springbootinit.common.ResultUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * 全局异常处理器
+ *
+ * @author <a href="https://github.com/liqmlx">程序员鱼皮</a>
+ * @from <a href="https://qmlx.icu">编程导航知识星球</a>
+ */
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BusinessException.class)
+    public BaseResponse<?> businessExceptionHandler(BusinessException e) {
+        log.error("BusinessException", e);
+        return ResultUtils.error(e.getCode(), e.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public BaseResponse<?> runtimeExceptionHandler(RuntimeException e) {
+        log.error("RuntimeException", e);
+        return ResultUtils.error(ErrorCode.SYSTEM_ERROR, "系统错误");
+    }
+}
