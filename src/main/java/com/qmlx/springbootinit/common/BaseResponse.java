311cbc19365ab9aa80a3f2ead67625fd345e5fb3
qmlx <2300604302@qq.com>
2024-04-01 22:51:02 +0800

    完成api签名认证

--- src/main/java/com/qmlx/springbootinit/common/BaseResponse.java
+++ src/main/java/com/qmlx/springbootinit/common/BaseResponse.java
@@ -12,6 +12,7 @@ public class BaseResponse<T> implements Serializable {
 
     private String message;
 
+
     public BaseResponse(int code, T data, String message) {
         this.code = code;
         this.data = data;
